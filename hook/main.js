5c2dbfd8e5f30ae9633fc0296f891ea27fb4bfb0
Travis Thieman <travis.thieman@gmail.com>
2014-03-22 18:28:38 -0400

    This is starting to look like a thing that does stuff

--- hook/main.js
+++ hook/main.js
@@ -1,10 +1,76 @@
 var express = require('express');
 var connect = require('connect');
+var db = require('monk')(process.env.OSSWEEPS_MONGO_HOST + '/data');
+
 var app = express();
 app.use(connect.bodyParser());
 
+function errorHandler(err, req, res, next) {
+  db.get('web_error').insert({
+    dt: new Date(),
+    stack: err.stack
+  });
+  res.status(500);
+  res.render('error', { error: err });
+}
+app.use(errorHandler);
+
+function processEntry(hookBody) {
+  if (hookBody.ref !== 'refs/heads/master' ||
+      hookBody.repository.private === true) {
+    return;
+  }
+
+  var users = {}
+  var numCommits = 0;
+
+  for (var i = 0; i < hookBody.commits.length; i++) {
+    if (hookBody.commits[i].distinct === false) {
+      continue;
+    }
+    var author = hookBody.commits[i].author;
+    var username = author.username;
+    users[username] = (users[username] || []) + 1;
+    numCommits++;
+  }
+
+  updateRepo(hookBody.repository, numCommits);
+  for (var user in users) {
+    updateUser(user, hookBody.repository, users[user]);
+  }
+}
+
+function updateRepo(repo, numCommits) {
+  db.get('data').update(
+    {_id: repo.id},
+    {
+      $set: {
+        name: repo.name,
+        url: repo.url
+      },
+      $inc: {entries: numCommits}
+    },
+    {upsert: true}
+  );
+}
+
+function updateUser(username, repo, numCommits) {
+  var repoKey = 'repos.' + repo.id;
+  db.get('data').update(
+    {_id: username},
+    {$inc: {repoKey: numCommits}}
+  );
+}
+
 app.post('/', function(req, res) {
-  console.log(req.body);
+  try { processEntry(req.body); }
+  catch (err) {
+    db.get('error').insert({
+      dt: new Date(),
+      body: req.body,
+      stack: err.stack
+    });
+  }
   res.end();
 });
 
